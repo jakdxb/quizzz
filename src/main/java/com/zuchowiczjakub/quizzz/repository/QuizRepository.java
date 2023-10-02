f5c9d96dcaf94f329a904fedee770d4aa47e57a9
zjakubb <jakub.zuchowicz@student.uj.edu.pl>
2023-10-02 04:11:20 +0200

    unnecessary comment

--- src/main/java/com/zuchowiczjakub/quizzz/repository/QuizRepository.java
+++ src/main/java/com/zuchowiczjakub/quizzz/repository/QuizRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    // Tutaj możesz dodać niestandardowe metody dostępu do bazy danych, jeśli są potrzebne.
+
 }
 
